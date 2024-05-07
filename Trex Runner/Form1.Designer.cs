373eb2aa23c8553a32d1d47c6b1f5cf096c4f7ac
Dell <Dell@DESKTOP-25O4IRQ>
2024-05-07 16:41:55 +0800

    Add project files.

---
+++ Trex Runner/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿using System.Windows.Forms;
+
+namespace Trex_Runner
+{
+    partial class formula
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtscore = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.trex = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.gamerTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trex)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtscore
+            // 
+            this.txtscore.AutoSize = true;
+            this.txtscore.Font = new System.Drawing.Font("Consolas", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtscore.Location = new System.Drawing.Point(12, 9);
+            this.txtscore.Name = "txtscore";
+            this.txtscore.Size = new System.Drawing.Size(150, 33);
+            this.txtscore.TabIndex = 0;
+            this.txtscore.Text = "Score : 0";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Black;
+            this.pictureBox1.Location = new System.Drawing.Point(-14, 416);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(888, 42);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // trex
+            // 
+            this.trex.Image = global::Trex_Runner.Properties.Resources.running;
+            this.trex.Location = new System.Drawing.Point(109, 347);
+            this.trex.Name = "trex";
+            this.trex.Size = new System.Drawing.Size(40, 43);
+            this.trex.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.trex.TabIndex = 2;
+            this.trex.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Trex_Runner.Properties.Resources.obstacle_1;
+            this.pictureBox2.Location = new System.Drawing.Point(619, 370);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(23, 46);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Tag = "obstacle";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::Trex_Runner.Properties.Resources.obstacle_2;
+            this.pictureBox3.Location = new System.Drawing.Point(738, 383);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(32, 33);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox3.TabIndex = 4;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Tag = "obstacle";
+            // 
+            // gamerTimer
+            // 
+            this.gamerTimer.Interval = 20;
+            this.gamerTimer.Tick += new System.EventHandler(this.MainGameTimerEvent);
+            // 
+            // formula
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(855, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.trex);
+            this.Controls.Add(this.txtscore);
+            this.Name = "formula";
+            this.Text = "Trex Runner";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyless);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyarrow);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trex)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void NewMethod()
+        {
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyless);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label txtscore;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox trex;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Timer gamerTimer;
+        private KeyEventHandler Keyisup;
+    }
+}
+
---
+++ Trex Runner/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Trex_Runner
+{
+    public partial class formula : Form
+    {
+        bool jumping = false;
+        int jumpSpeed;
+        int force = 0;
+        int score = 0;
+        int obstaclespeed = 10;
+        Random rand = new Random();
+        int position;
+        bool isGameOver = false;
+
+       
+        
+        public formula()
+        {
+            InitializeComponent();
+
+            GameReset();
+        }
+
+        private void MainGameTimerEvent(object sender, EventArgs e)
+        {
+            trex.Top += jumpSpeed;
+
+            txtscore.Text = "Score: " + score;
+
+            if (jumping == true && force < -5)
+            {
+                jumping = false;
+            }
+
+            if (jumping == true)
+            {
+                jumpSpeed = -12;
+                force -= 2;
+            }
+            else
+            {
+                jumpSpeed = 12;
+            }
+
+
+            if (trex.Top > 297 && jumping == false)
+            {
+                force = 13;
+                trex.Top = 298;
+                jumpSpeed = 0;
+            }
+
+
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && (string)x.Tag == "obstacle")
+                {
+                    x.Left -= obstaclespeed;
+
+                    if (x.Left < -100)
+                    {
+                        x.Left = ClientSize.Width + rand.Next(200, 500) + (x.Width * 15);
+                        score++;
+                    }
+
+                    if (trex.Bounds.IntersectsWith(x.Bounds))
+                    {
+                        gamerTimer.Stop();
+                        trex.Image = Properties.Resources.dead;
+                        txtscore.Text += "    Press R to restart the game!";
+                        isGameOver = true;
+                    }
+                }
+            }
+
+            if (score > 10)
+            {
+                obstaclespeed = 10;
+            }
+        }
+
+        private void keyless(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space && jumping == false)
+            {
+                jumping =true;
+            }
+        }
+
+        private void keyarrow(object sender, KeyEventArgs e)
+        {
+            if (jumping == true)
+            {
+                jumping = false;
+            }
+
+            if (e.KeyCode == Keys.R && isGameOver == true)
+            {
+                GameReset();
+            }
+        }
+
+        private void GameReset()
+        {
+            force =13;
+            jumpSpeed = 0;
+            jumping = false;
+            score = 0;
+            obstaclespeed = 10;
+            txtscore.Text = "Score: " + score;
+            trex.Image = Properties.Resources.running;
+            isGameOver = false;
+            trex.Top = 298;
+
+            foreach (Control x in this.Controls)
+            {
+
+                if (x is PictureBox && (string)x.Tag == "obstacle")
+                {
+                 position = this.ClientSize.Width + rand.Next(200, 500) + (x.Width * 10);
+
+                    x.Left = position;
+                }
+            }
+
+            gamerTimer.Start();
+
+        }
+
+        
+    }
+}
